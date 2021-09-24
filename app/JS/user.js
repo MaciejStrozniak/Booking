96a519d34d36581c0080136c8d7c6226297462d3
MaciejStrozniak <maciejstrozniak@gmail.com>
2021-09-24 12:48:08 +0200

    Create user Client class first try to build server

--- app/JS/user.js
+++ app/JS/user.js
@@ -1,10 +1,41 @@
-// linking functions to Booking buttons
+// ------------ Create booking object holding all user data ----------
+
+class Client
+{
+    constructor(name, surname, email, mobilePhone)
+    {
+        this.name;
+        this.surname;
+        this.email;
+        this.mobilePhone;
+    }
+
+    saveData() {
+        this.name = nameTextElement.value;
+        this.surname = surnameTextElement.value;
+        this.email = emailTextElement.value;
+        this.mobilePhone = mobileTextElement.value;
+    }
+}
+
+// ----------- Linking functions to DOM -------------
 
 let modal = document.getElementById('myModal');
 let overlay = document.getElementById('overlay');
 let bookingButton = document.querySelectorAll('[data-booking]'); // this is an array now
 let closeBtn = document.querySelector('[data-close]');
-let inputText = document.getElementById('input');
+let confirmBtn = document.getElementById('input');
+// let confirmBtn = document.querySelector('[data-confirm]');
+
+let nameTextElement = document.getElementById('name');
+let surnameTextElement = document.getElementById('surname');
+let emailTextElement = document.getElementById('email');
+let mobileTextElement = document.getElementById('mobile');
+// let testBtn = document.querySelector('[data-test]');
+
+// ------------ creating client object -----------------
+
+const client = new Client(nameTextElement, surnameTextElement, emailTextElement, mobileTextElement); 
 
 // activating functions linked to buttons
 
@@ -32,3 +63,15 @@ closeBtn.addEventListener('click', () => {
     }
         
 });
+
+confirmBtn.addEventListener('click', () => {
+    client.saveData();
+    console.log(client.name);
+});
+
+// testBtn.addEventListener('click', () => {
+//     console.log(client.name);
+//     console.log(client.surname);
+//     console.log(client.email);
+//     console.log(client.mobilePhone);
+// });
